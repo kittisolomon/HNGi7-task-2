8160169a3163a28bc2068d57a919cb29391770aa
Kitti solomon <40053238+kittisolomon@users.noreply.github.com>
2020-06-04 23:22:23 +0100

    Create kittison-HNG-04815.js

---
+++ scripts/kittison-HNG-04815.js
@@ -0,0 +1,13 @@
+let internDetails= {
+
+fullName: "Kitti Solomon ", 
+
+email:"officialkittisolomon@gmail.com",
+
+hngi7ID: "HNG-04815",
+
+language: "Javascript"
+
+};
+
+document.write ("Hello World, this is " + internDetails.fullName + " with HNG ID " + internDetails.hngi7ID + " using " + internDetails.language + " for stage 2 task");
